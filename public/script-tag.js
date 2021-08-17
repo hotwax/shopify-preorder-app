8c2a828c9d08da00247dfd03d8dcea044521484e
Yash Maheshwari <yash.maheshwari@hotwax.co>
2021-08-17 14:25:05 +0530

    Added: the script for the pre-order app and gitignore file(shopify-preorder#01)

---
+++ public/script-tag.js
@@ -0,0 +1,135 @@
+
+  (function () {
+    let jQueryBopis;
+
+    this.preOrderCustomConfig = {
+        'enableCartRedirection': true
+    };
+
+    // TODO Generate instance specific code URL in FTL. Used with <#noparse> after this code so that `` code is escaped
+    // let baseUrl = '<@ofbizUrl secure="true"></@ofbizUrl>';
+    let baseUrl = 'https://dev-hc.hotwax.io';
+    // let baseUrl = 'https://demo-hc.hotwax.io';
+
+    let loadScript = function(url, callback){
+
+        let script = document.createElement("script");
+        script.type = "text/javascript";
+
+        if (script.readyState){ 
+            script.onreadystatechange = function(){
+                if (script.readyState == "loaded" || script.readyState == "complete"){
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else {
+            script.onload = function(){
+                callback();
+            };
+        }
+    
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+        
+    };
+
+    if ((typeof jQuery === 'undefined') || (parseFloat(jQuery.fn['jquery']) < 1.7)) {
+        loadScript('//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', function(){
+          jQueryBopis = jQuery.noConflict(true);
+          jQueryBopis(document).ready(function() {
+              initialisePreOrder();
+          });
+
+        });
+    } else {
+        jQueryBopis = jQuery;
+        jQuery(document).ready(function() {
+            initialisePreOrder();
+        });
+    }
+
+    function checkPreOrder (id) {
+        return new Promise(function(resolve, reject) {
+            jQueryBopis.ajax({
+                type: 'POST',
+                url: `${baseUrl}/api/searchProducts`,
+                data: JSON.stringify({
+                    "filters": [ `sku: ${id}` ]
+                }),
+                dataType: "json",
+                crossDomain: true,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                success: function (data) {
+                    console.log(data)
+                    resolve(data)
+                },
+                error: function (err) {
+                    console.log(err)
+                    reject(err)
+                }
+            })
+        })
+    }
+
+    async function initialisePreOrder () {
+      
+        if (location.pathname.includes('products')) {
+            const cartForm = jQueryBopis("form[action='/cart/add']");
+            const id = cartForm.serializeArray().find(ele => ele.name === "id").value;
+            const addToCartButton = jQueryBopis("form[action^='/cart/add']:first [type=submit]:visible:first");
+            const ifPreOrderActive = pn.settings.singleProductSettings.some((ele) => {
+                return ele['n'] === id;
+            });
+
+            const sku = meta.product.variants.find(variant => variant.id == id).sku
+            checkPreOrder(sku);
+
+            if (ifPreOrderActive) {
+                // addToCart.attr("aria-label","Pre Order");
+                // addToCart.val("Pre Order");
+                addToCartButton.html("Pre Order");
+                // addToCartButton.on("click", addToCart.bind(null));
+            }
+        }
+    }
+
+    function addToCart(event) {
+
+        let addToCartForm = jQueryBopis("form[action='/cart/add']");
+
+        event.preventDefault();
+        event.stopImmediatePropagation();
+
+        let preOrderProperty = jQueryBopis(`<input id="pre-order-item" name="properties[Note]" value="Pre-Order" type="hidden"/>`)
+        addToCartForm.append(preOrderProperty)
+
+        // using the cart add endpoint to add the product to cart, as using the theme specific methods is not recommended.
+        jQueryBopis.ajax({
+            type: "POST",
+            url: '/cart/add.js',
+            data: addToCartForm.serialize(),
+            dataType: 'JSON',
+            success: function () {
+                // redirecting the user to the cart page after the product gets added to the cart
+                if (preOrderCustomConfig.enableCartRedirection) {
+                    location.replace('/cart');
+                }
+            }
+        })
+
+        preOrderProperty.remove();
+    }
+
+    // TODO move it to intialise block
+    // To check whether the url has changed or not, for making sure that the variant is changed.
+    let url = location.href; 
+    new MutationObserver(() => {
+        if (location.href !== url) {
+            url = location.href;
+            initialisePreOrder();
+        }
+    }).observe(document, {subtree: true, childList: true});
+})();
