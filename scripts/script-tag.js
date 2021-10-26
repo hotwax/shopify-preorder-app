bf6ea4a37179b67970c8803cf3a5d02b4e60b2f7
Yash Maheshwari <yash.maheshwari@hotwax.co>
2021-10-26 14:54:58 +0530

    Removed: hardcoded values from the script(#e5d9vw)

--- scripts/script-tag.js
+++ scripts/script-tag.js
@@ -62,7 +62,7 @@
                 url: `${baseUrl}/api/checkPreorderItemAvailability`,
                 data: JSON.stringify({
                     "filters": {
-                        "sku": ["31285153595519", "31285153562751"],
+                        "sku": ids,
                         "sku_op": "in"
                     }
                 }),
@@ -105,8 +105,7 @@
             if (preOrderDetails && preOrderDetails.count > 0) {
 
                 // iterating over the response to check for the current variant selected
-                const currentVariant = preOrderDetails.docs[0];
-                // const currentVariant = preOrderDetails.docs.find((product) => product.productId === id)
+                const currentVariant = preOrderDetails.docs.find((product) => product.productId === id)
 
                 if (currentVariant) {
                     const deliveryDate = moment(currentVariant.estimatedDeliveryDate).format("YYYY-MM-DD hh:mm:ss A")
