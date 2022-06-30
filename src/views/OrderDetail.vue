414adbf370d25543ffd20cb68351d85a5e7cc46f
Disha Talreja <dishatalreja1202@gmail.com>
2022-06-30 15:05:49 +0530

    Improved indentation(#2fz56t8)

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -8,64 +8,64 @@
     </ion-header>
     <ion-content>
       <div>
-      <ion-list>
-        <ion-item lines="none">
-          <ion-label>
-            <h2>{{order.customer?.first_name}} {{ order.customer?.last_name }}</h2>
-            <p>{{ order.name }}</p>
-          </ion-label>
-          <ion-badge color="dark" slot="end">{{ order.created_at }}</ion-badge>
-        </ion-item>
-      </ion-list>
-      <ion-item v-if="order.customer?.phone">
-        <ion-icon :icon="callOutline" slot="start" />
-        <ion-label>{{ order.customer?.phone }}</ion-label>
-        <ion-button
-          fill="outline"
-          slot="end"
-          color="medium"
-        >
-          {{ $t("Copy") }}
-        </ion-button>
-      </ion-item>
-      <ion-item lines="none" v-if="order.customer?.email">
-        <ion-icon :icon="mailOutline" slot="start" />
-        <ion-label>{{ order.customer?.email }}</ion-label>
-        <ion-button
-          fill="outline"
-          slot="end"
-          color="medium"
-        >
-          {{ $t("Copy") }}
-        </ion-button>
-      </ion-item>
-  
-      <main v-for="item in order.line_items" :key="item.id">
-        <ion-card>
+        <ion-list>
           <ion-item lines="none">
             <ion-label>
-              <h2>{{ item.title }}</h2>
-              <p>{{ item.variant_title }}</p>
-              <p class="ion-text-wrap">{{ $t("SKU") }}: {{ item.sku }}</p>
+              <h2>{{order.customer?.first_name}} {{ order.customer?.last_name }}</h2>
+              <p>{{ order.name }}</p>
             </ion-label>
+            <ion-badge color="dark" slot="end">{{ order.created_at }}</ion-badge>
           </ion-item>
-          <ion-item>
-            <ion-label v-for="property in item.properties" :key="property">
-              <p>{{ property.name }}: {{ property.value }}</p>
-            </ion-label>
-          </ion-item>
-          <ion-item lines="none" class="border-top">
-            <ion-label>{{ $t("Pre Order") }}</ion-label>
-            <ion-checkbox :checked="isChecked(item, 'Pre Order')" @ionChange="addProperty('Pre Order', item, $event)" />
-          </ion-item>
-          <ion-item lines="none" class="border-top">
-            <ion-label>{{ $t("Back Order") }}</ion-label>
-            <ion-checkbox :checked="isChecked(item, 'Back Order')" @ionChange="addProperty('Back Order', item, $event)" />
-          </ion-item>               
-        </ion-card>
-      </main>
-      <ion-button @click="updateOrder(order.line_items)">{{ $t("Update") }}</ion-button> 
-    </div>   
+        </ion-list>
+        <ion-item v-if="order.customer?.phone">
+          <ion-icon :icon="callOutline" slot="start" />
+          <ion-label>{{ order.customer?.phone }}</ion-label>
+          <ion-button
+            fill="outline"
+            slot="end"
+            color="medium"
+          >
+            {{ $t("Copy") }}
+          </ion-button>
+        </ion-item>
+        <ion-item lines="none" v-if="order.customer?.email">
+          <ion-icon :icon="mailOutline" slot="start" />
+          <ion-label>{{ order.customer?.email }}</ion-label>
+          <ion-button
+            fill="outline"
+            slot="end"
+            color="medium"
+          >
+            {{ $t("Copy") }}
+          </ion-button>
+        </ion-item>
+    
+        <main v-for="item in order.line_items" :key="item.id">
+          <ion-card>
+            <ion-item lines="none">
+              <ion-label>
+                <h2>{{ item.title }}</h2>
+                <p>{{ item.variant_title }}</p>
+                <p class="ion-text-wrap">{{ $t("SKU") }}: {{ item.sku }}</p>
+              </ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label v-for="property in item.properties" :key="property">
+                <p>{{ property.name }}: {{ property.value }}</p>
+              </ion-label>
+            </ion-item>
+            <ion-item lines="none" class="border-top">
+              <ion-label>{{ $t("Pre Order") }}</ion-label>
+              <ion-checkbox :checked="isChecked(item, 'Pre Order')" @ionChange="addProperty('Pre Order', item, $event)" />
+            </ion-item>
+            <ion-item lines="none" class="border-top">
+              <ion-label>{{ $t("Back Order") }}</ion-label>
+              <ion-checkbox :checked="isChecked(item, 'Back Order')" @ionChange="addProperty('Back Order', item, $event)" />
+            </ion-item>
+          </ion-card>
+        </main>
+        <ion-button @click="updateOrder(order.line_items)">{{ $t("Update") }}</ion-button>
+      </div>
     </ion-content>
   </ion-page>
 </template>
